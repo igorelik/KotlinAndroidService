1e900e3a6be51d040af5890888b00d437b7f0ca8
Igor Gorelik <gorelik.igor@gmail.com>
2018-10-02 09:28:45 +1000

    Add AppCenter integration

--- app/src/main/java/com/example/igorg/kpollingservice/MainActivity.kt
+++ app/src/main/java/com/example/igorg/kpollingservice/MainActivity.kt
@@ -5,12 +5,19 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.AppCenter
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // setContentView(R.layout.activity_main)
+        AppCenter.start(application, "3fd20389-8835-43de-857b-4aa94895428e",
+                Analytics::class.java, Crashes::class.java)
         if(!PollingService.IsRunning)
         {
             var intent = Intent(this, PollingService::class.java)
